6b1451a47de6d4c5fa4f8464f7e6800fce385a70
Michel-Rooney <michelrooney16@gmail.com>
2023-09-22 18:46:19 -0300

    FEAT: Inicializando a Page News

--- distros-linux/src/screens/News.js
+++ distros-linux/src/screens/News.js
@@ -1,3 +1,5 @@
+import '../css/News.css';
+
 function News () {
   return (
     <main class="main">
@@ -5,7 +7,66 @@ function News () {
         <h1 class="title">News</h1>
       </div>
       <div class="container">
-        
+        <div className='News-section-start'>
+          <section>
+            <img src='http://placehold.it/450x250' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </section>
+          <aside>
+            <div className='News-aside-box'>
+              <img src='http://placehold.it/150x150' alt='Random'/>
+              <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+            </div>
+            <div className='News-aside-box News-box-reverse'>
+              <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+              <img src='http://placehold.it/150x150' alt='Random'/>
+            </div>
+          </aside>
+        </div>
+        <section className='News-section'>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+        </section>
+        {/* <section className='News-section'>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+          <div>
+            <img src='http://placehold.it/250x150' alt='Random'/>
+            <p>Como a Tecnologia Está Transformando a Maneira como Nos Movemos</p>
+          </div>
+        </section> */}
       </div>
     </main>
   )
